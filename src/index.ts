217fd0e6bdca7ad37480156d08382286eb2fadac
sayedayman203 <sayedayman203@gmail.com>
2023-03-09 16:15:18 +0200

    update packages, active search event

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-export { NgSelectComponent, NG_SELECT_DEFAULT_CONFIG } from './ng-select/ng-select.component';
-export { NgSelectModule } from './ng-select/ng-select.module';
-export { NgOption, NgSelectConfig } from './ng-select/ng-select.types';
+export {
+  NgxSelectV1Component,
+  NG_SELECT_DEFAULT_CONFIG,
+} from "./ng-select/ng-select.component";
+export { NgxSelectV1Module } from "./ng-select/ng-select.module";
+export { NgOption, NgSelectConfig } from "./ng-select/ng-select.types";
--- src/ng-select/ng-select.module.ts
+++ src/ng-select/ng-select.module.ts
@@ -1,75 +1,76 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NgSelectComponent, NG_SELECT_DEFAULT_CONFIG } from './ng-select.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import {
+  NgxSelectV1Component,
+  NG_SELECT_DEFAULT_CONFIG,
+} from "./ng-select.component";
+import {
+  NgOptionTemplateDirective,
+  NgLabelTemplateDirective,
+  NgHeaderTemplateDirective,
+  NgFooterTemplateDirective,
+  NgOptgroupTemplateDirective,
+  NgNotFoundTemplateDirective,
+  NgTypeToSearchTemplateDirective,
+  NgLoadingTextTemplateDirective,
+  NgMultiLabelTemplateDirective,
+  NgTagTemplateDirective,
+} from "./ng-templates.directive";
+import { NgxOptionV1Component } from "./ng-option.component";
+import { NgOptionHighlightDirective } from "./ng-option-highlight.directive";
+import { NgDropdownPanelComponent } from "./ng-dropdown-panel.component";
+import { WindowService } from "./window.service";
+import { VirtualScrollService } from "./virtual-scroll.service";
+import { ConsoleService } from "./console.service";
+
+@NgModule({
+  declarations: [
+    NgDropdownPanelComponent,
+    NgxOptionV1Component,
+    NgxSelectV1Component,
+    NgOptionHighlightDirective,
+    NgOptgroupTemplateDirective,
     NgOptionTemplateDirective,
     NgLabelTemplateDirective,
+    NgMultiLabelTemplateDirective,
     NgHeaderTemplateDirective,
     NgFooterTemplateDirective,
-    NgOptgroupTemplateDirective,
     NgNotFoundTemplateDirective,
     NgTypeToSearchTemplateDirective,
     NgLoadingTextTemplateDirective,
+    NgTagTemplateDirective,
+  ],
+  imports: [CommonModule],
+  exports: [
+    NgxSelectV1Component,
+    NgxOptionV1Component,
+    NgOptionHighlightDirective,
+    NgOptgroupTemplateDirective,
+    NgOptionTemplateDirective,
+    NgLabelTemplateDirective,
     NgMultiLabelTemplateDirective,
-    NgTagTemplateDirective
-} from './ng-templates.directive';
-import { NgOptionComponent } from './ng-option.component';
-import { NgOptionHighlightDirective } from './ng-option-highlight.directive' ;
-import { NgDropdownPanelComponent } from './ng-dropdown-panel.component';
-import { WindowService } from './window.service';
-import { VirtualScrollService } from './virtual-scroll.service';
-import { ConsoleService } from './console.service';
-
-@NgModule({
-    declarations: [
-        NgDropdownPanelComponent,
-        NgOptionComponent,
-        NgSelectComponent,
-        NgOptionHighlightDirective,
-        NgOptgroupTemplateDirective,
-        NgOptionTemplateDirective,
-        NgLabelTemplateDirective,
-        NgMultiLabelTemplateDirective,
-        NgHeaderTemplateDirective,
-        NgFooterTemplateDirective,
-        NgNotFoundTemplateDirective,
-        NgTypeToSearchTemplateDirective,
-        NgLoadingTextTemplateDirective,
-        NgTagTemplateDirective
-    ],
-    imports: [
-        CommonModule
-    ],
-    exports: [
-        NgSelectComponent,
-        NgOptionComponent,
-        NgOptionHighlightDirective,
-        NgOptgroupTemplateDirective,
-        NgOptionTemplateDirective,
-        NgLabelTemplateDirective,
-        NgMultiLabelTemplateDirective,
-        NgHeaderTemplateDirective,
-        NgFooterTemplateDirective,
-        NgNotFoundTemplateDirective,
-        NgTypeToSearchTemplateDirective,
-        NgLoadingTextTemplateDirective,
-        NgTagTemplateDirective
-    ],
-    providers: [
-        ConsoleService,
-        WindowService,
-        VirtualScrollService,
-        {
-            provide: NG_SELECT_DEFAULT_CONFIG,
-            useValue: {
-                notFoundText: 'No items found',
-                typeToSearchText: 'Type to search',
-                addTagText: 'Add item',
-                loadingText: 'Loading...',
-                clearAllText: 'Clear all',
-                disableVirtualScroll: false
-            }
-        }
-    ]
+    NgHeaderTemplateDirective,
+    NgFooterTemplateDirective,
+    NgNotFoundTemplateDirective,
+    NgTypeToSearchTemplateDirective,
+    NgLoadingTextTemplateDirective,
+    NgTagTemplateDirective,
+  ],
+  providers: [
+    ConsoleService,
+    WindowService,
+    VirtualScrollService,
+    {
+      provide: NG_SELECT_DEFAULT_CONFIG,
+      useValue: {
+        notFoundText: "No items found",
+        typeToSearchText: "Type to search",
+        addTagText: "Add item",
+        loadingText: "Loading...",
+        clearAllText: "Clear all",
+        disableVirtualScroll: false,
+      },
+    },
+  ],
 })
-export class NgSelectModule { }
+export class NgxSelectV1Module {}
